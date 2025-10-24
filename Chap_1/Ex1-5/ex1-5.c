62dc22afed2b067e76f41dd1dfa951be26f7d32f
daxpatel21 <your.email@example.com>
2025-10-24 13:29:22 +0530

    ex1-5 added

--- Chap_1/Ex1-5/ex1-5.c
+++ Chap_1/Ex1-5/ex1-5.c
@@ -2,8 +2,12 @@
 
 int main(){
 	int fahr;
-    for(fahr = 300; fahr <= 0; fahr = fahr - 20)
+    printf("-------------------------------------\n");
+    printf("Fahrenheit To Celsius Converter Table\n");
+    printf("-------------------------------------\n");
+    for(fahr = 300; fahr <= 0; fahr = fahr - 20){
         printf("%3d\ %6.1f\n", fahr , (5.0/9.0) * (fahr - 32.0));
+    }
 	return 0;
 }
 
