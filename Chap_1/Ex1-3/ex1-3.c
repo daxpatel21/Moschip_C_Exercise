7495bd11aad80496faa9f94f5b922a4318eb1f07
daxpatel21 <your.email@example.com>
2025-10-24 11:16:21 +0530

    ex1-3 added

--- Chap_1/Ex1-3/ex1-3.c
+++ Chap_1/Ex1-3/ex1-3.c
@@ -4,8 +4,8 @@ int main() {
     printf("------------------------------------\n");
     printf("Fahrenhit To Celsius Converter Table\n");
     printf("------------------------------------\n");
-    int fahr,celsius;
-    int lower,upper,step;
+    float fahr,celsius;
+    float lower,upper,step;
 
     lower = 0;
     upper = 300;
@@ -13,8 +13,8 @@ int main() {
 
     fahr = lower;
     while (fahr <= upper) {
-        celsius = 5 * (fahr - 32) / 9;
-        printf("%3d\t%6d\n",fahr,celsius);
+        celsius = (5.0/9.0) * (fahr - 32.0);
+        printf("%3.0f\t%6.1f\n",fahr,celsius);
         fahr = fahr + step;
     }
     return 0;
