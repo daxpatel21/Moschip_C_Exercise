de93e24c889c68fec16d8904a9b9884d219dd2b3
daxpatel21 <your.email@example.com>
2025-10-24 11:29:40 +0530

    Makefile added

--- Chap_1/Ex1-4/ex1-4.c
+++ Chap_1/Ex1-4/ex1-4.c
@@ -4,18 +4,17 @@ int main() {
     printf("-------------------------------------\n");
     printf("Celsius To Fahrenheit Converter Table\n");
     printf("-------------------------------------\n");
-
-    float fahr,celsius;
-    float lower,upper,step;
+    float fahr, celsius;
+    float lower, upper, step;
 
     lower = 0;
     upper = 300;
     step = 20;
 
-    fahr = lower;
+    celsius = lower;
     while (celsius <= upper) {
         fahr = (celsius * (9.0/5.0)) + 32.0;
-        printf("%3.0f\t%6.1f\n",celsius,fahr);
+        printf("%6.0f\t%12.0f\n",celsius,fahr);
         celsius = celsius + step;
     }
     return 0;
